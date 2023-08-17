2f9102c02cef6275cfac4bb83058edfef3b94d11
Tony Issakov <tonyissakov@gmail.com>
2023-08-17 06:42:03 -0700

    strip down and resort order. add custom nodes for conveniencef

--- official-templates/stable-diffusion-comfyui/pre_start.sh
+++ official-templates/stable-diffusion-comfyui/pre_start.sh
@@ -2,8 +2,5 @@
 
 export PYTHONUNBUFFERED=1
 source /venv/bin/activate
-rsync -au --remove-source-files /ComfyUI/* /workspace/ComfyUI
-ln -s /comfy-models/* /workspace/ComfyUI/models/checkpoints/
-
 cd /workspace/ComfyUI
 python main.py --listen --port 3000 &
